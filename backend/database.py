38c556a104ae0fe3a0cd729b9c3521ec27d5b5f7
Hanif Kolahdoozan <hanif.k@gmail.com>
2025-03-22 10:15:53 -0400

    Swithched to PostgreSQL on Render

--- backend/database.py
+++ backend/database.py
@@ -2,12 +2,16 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from models import Base
 
-DATABASE_URL = "sqlite:///./workout.db"
+DATABASE_URL = "postgresql://hanif:YbA3QHGX8CYNR2WtJYKfzzsx6Z4HpzaI@dpg-cvfc81ggph6c73bbi1lg-a/vftest"
 
-engine = create_engine(
-    DATABASE_URL, connect_args={"check_same_thread": False}
-)
+#create database engine
+engine = create_engine(DATABASE_URL)
 
+#create session local (for interacting with the database)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
+#delare base class for ORM models
+Base = declarative_base()
+
+#create all table in the database (if not exists)
 Base.metadata.create_all(bind=engine)
