2a244c42034214f7832fa24041a3d5d343b39a25
Hanif Kolahdoozan <hanif.k@gmail.com>
2025-03-24 16:24:35 -0400

    Fix render PostgreSQL connection string with SSL

--- backend/database.py
+++ backend/database.py
@@ -3,7 +3,7 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
 
 # Put your exact connection string here for testing
-DATABASE_URL = "postgresql://user123:password123@dpg-cvfc81ggph6c73bbi1lg-a.oregon-postgres.render.com:5432/voice_fitness_db"
+DATABASE_URL = "postgresql://hanif:YbA3QHGX8CYNR2WtJYKfzzsx6Z4HpzaI@dpg-cvfc81ggph6c73bbi1lg-a.oregon-postgres.render.com/vftest"
 
 engine = create_engine(DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
