4a7845de2877aeea8a42c1ada3ee027e397cf416
Hanif Kolahdoozan <hanif.k@gmail.com>
2025-03-26 12:30:31 -0400

    Add hashing.py for password hashing and verfication

---
+++ backend/hashing.py
@@ -0,0 +1,12 @@
+from passlib.context import CryptContext
+
+# Define a hashing context using bcrypt
+pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
+
+# Function to hash passwords
+def hash_password(password: str) -> str:
+    return pwd_context.hash(password)
+
+# Function to verify passwords
+def verify_password(plain_password: str, hashed_password: str) -> bool:
+    return pwd_context.verify(plain_password, hashed_password)
